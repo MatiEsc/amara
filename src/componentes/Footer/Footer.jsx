27fcd66d394e8cdea173afc706db8975f4a2b09f
Matias Escobar <matias.e.escobar@gmail.com>
2024-05-18 09:13:02 -0300

    empezando con el novBar y el footer

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -1,7 +1,46 @@
 import "./Footer.css";
 
 const Footer = () => {
-  return <footer className="footer-menu"></footer>;
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-section">
+          <h4>Contacto</h4>
+          <p>Tel: +1 234 567 890</p>
+          <p>Email: contacto@tienda-amara.com</p>
+        </div>
+        <div className="footer-section">
+          <h4>Enlaces</h4>
+          <ul>
+            <li>
+              <a href="#home">Inicio</a>
+            </li>
+            <li>
+              <a href="#about">Sobre Nosotros</a>
+            </li>
+            <li>
+              <a href="#services">Servicios</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>Síguenos</h4>
+          <div className="social-icons">
+            <a href="https://facebook.com" className="social-icon">
+              Facebook
+            </a>
+            <a href="https://twitter.com" className="social-icon">
+              Twitter
+            </a>
+            <a href="https://instagram.com" className="social-icon">
+              Instagram
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">&copy; 2023 Tienda Amara. Todos los derechos reservados.</div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,57 +1,29 @@
-import { useState, useEffect } from "react";
-
-import { Link } from "react-router-dom";
-
 import "./NavBar.css";
 
 const NavBar = () => {
-  const [menuVisible, setMenuVisible] = useState(false);
-
-  useEffect(() => {
-    const toggleButton = document.getElementById("_toggle");
-    const itemsContainer = document.getElementById("_items");
-
-    const handleToggleClick = () => {
-      itemsContainer.classList.toggle("open");
-      toggleButton.classList.toggle("close");
-    };
-
-    toggleButton.addEventListener("click", handleToggleClick);
-
-    return () => {
-      toggleButton.removeEventListener("click", handleToggleClick);
-    };
-  }, []);
-
   return (
-    <nav id="navbar">
-      <Link to="/" className="brand">
-        <img src="" alt="" />
-      </Link>
-      <ul className="nav_items" id="_items">
-        <li>
-          <Link to=""></Link>
-        </li>
-        <li>
-          <Link to=""></Link>
-        </li>
-        <li>
-          <Link to=""></Link>
-        </li>
-        <li>
-          <Link to=""> </Link>
-        </li>
-        <li>
-          <Link to=""> </Link>
-        </li>
-      </ul>
-
-      <div className="nav_toggle" id="_toggle">
-        <span></span>
-        <span></span>
-        <span></span>
+    <header className="header">
+      <div className="logo-container">
+        <img src="/public/img/navBar/logoNavbar.png" alt="Logo" className="logo" />
+        <p className="brand-name">AMARA</p>
       </div>
-    </nav>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Inicio</a>
+          </li>
+          <li>
+            <a href="#productos">Productos</a>
+          </li>
+          <li>
+            <a href="#servico">Servicios</a>
+          </li>
+          <li>
+            <a href="#contacto">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
