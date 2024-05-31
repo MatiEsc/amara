6f7653a879208d3ed0e9a4d6308b5fda637da7d1
Matias Escobar <matias.e.escobar@gmail.com>
2024-05-31 20:53:19 -0300

    Empezando la version mobile

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,6 +1,26 @@
+import { useState, useEffect } from "react";
+
 import "./NavBar.css";
 
 const NavBar = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleButton = document.getElementById("_toggle");
+    const itemsContainer = document.getElementById("_items");
+
+    const handleToggleClick = () => {
+      itemsContainer.classList.toggle("open");
+      toggleButton.classList.toggle("close");
+    };
+
+    toggleButton.addEventListener("click", handleToggleClick);
+
+    return () => {
+      toggleButton.removeEventListener("click", handleToggleClick);
+    };
+  }, []);
+
   const handleClick = (event, targetId) => {
     event.preventDefault();
     const targetElement = document.getElementById(targetId);
@@ -10,35 +30,39 @@ const NavBar = () => {
   };
 
   return (
-    <header className="header">
+    <nav id="navbar">
       <div className="logo-container">
         <img src="/img/navBar/logoNavDesk.png" alt="Logo" className="logo" />
       </div>
-      <nav>
-        <ul>
-          <li>
-            <a href="#home" onClick={(e) => handleClick(e, "home")}>
-              Inicio
-            </a>
-          </li>
-          <li>
-            <a href="#productos" onClick={(e) => handleClick(e, "productos")}>
-              Productos
-            </a>
-          </li>
-          <li>
-            <a href="#servicios" onClick={(e) => handleClick(e, "servicios")}>
-              Servicios
-            </a>
-          </li>
-          <li>
-            <a href="#contacto" onClick={(e) => handleClick(e, "contacto")}>
-              Contacto
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      <ul className="nav_items" id="_items">
+        <li>
+          <a href="#home" onClick={(e) => handleClick(e, "home")}>
+            Inicio
+          </a>
+        </li>
+        <li>
+          <a href="#productos" onClick={(e) => handleClick(e, "productos")}>
+            Productos
+          </a>
+        </li>
+        <li>
+          <a href="#servicios" onClick={(e) => handleClick(e, "servicios")}>
+            Servicios
+          </a>
+        </li>
+        <li>
+          <a href="#contacto" onClick={(e) => handleClick(e, "contacto")}>
+            Contacto
+          </a>
+        </li>
+      </ul>
+
+      <div className="nav_toggle" id="_toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
   );
 };
 
---
+++ src/componentes/NavBar/NavBar1.jsx
@@ -0,0 +1,45 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  const handleClick = (event, targetId) => {
+    event.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img src="/img/navBar/logoNavDesk.png" alt="Logo" className="logo" />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home" onClick={(e) => handleClick(e, "home")}>
+              Inicio
+            </a>
+          </li>
+          <li>
+            <a href="#productos" onClick={(e) => handleClick(e, "productos")}>
+              Productos
+            </a>
+          </li>
+          <li>
+            <a href="#servicios" onClick={(e) => handleClick(e, "servicios")}>
+              Servicios
+            </a>
+          </li>
+          <li>
+            <a href="#contacto" onClick={(e) => handleClick(e, "contacto")}>
+              Contacto
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
